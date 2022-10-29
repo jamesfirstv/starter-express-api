6da86fb7715333b72251b7a0ae9e6c3504e3e321
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-10-30 01:35:53 +0200

    update app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.listen(process.env.PORT || 3000)
 // Функция main()
 function main(req, res) {
   res.send('Yo!')
+  json_encode(array("replies" => array(
+		array("message" => "Hey " . $sender . "!\nThanks for sending: " . $message),
+		array("message" => "Success ✅")
+	)));
   console.log(req.body)
 //  res.send(JSON.stringify(req))
 }
