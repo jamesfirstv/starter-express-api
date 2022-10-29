4d8186d9bd7aaa098028aabd962f3cb118c8bb83
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-10-30 01:03:10 +0200

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app               = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.get('*', main)
-app.post(‘*’, main)
+//app.post(‘*’, main)
 
 // Функция main()
 function main(req, res) {
