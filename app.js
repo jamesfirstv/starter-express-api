67f2b99a3fcfae1471a67e10bf58e1617ea6c5af
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-10-29 18:12:48 +0200

    Move controller

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Старт сервера
-const app = require('express')()
-app.all('/', (req, res) => {
+const srv = require('express')()
+srv.all('/mrwtd', (req, res) => {
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+srv.listen(process.env.PORT || 3000)
