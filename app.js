c8661867db111453c3516ce4b0f698a6d2da6998
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-10-29 18:21:14 +0200

    main()

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-// Старт сервера
-const srv = require('express')()
-srv.all('/mrwtd', (req, res) => {
-    res.send('Yo!')
-})
-srv.listen(process.env.PORT || 3000)
+// Подготовка сервера
+const srv = require('express')()     // Загрузка либы Express
+srv.all('/mrwtd', main)              // Обрабатывать будем запросы на /mrwtd ("Message reserved, what to do?")
+srv.listen(process.env.PORT || 3000) // Стартуем сервак
+
+// Функция main()
+function main(req, res) {
+  res.send('Yo!')
+}
