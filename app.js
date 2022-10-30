4abf8778d3ac8ba7ce9f86e0da228c8d8478c1f5
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-10-30 03:45:32 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 function main(msg) {
+  switch (msg) {
+    case /./:
+  }
   return msg+1
 }
 
+// --------------------
+// Ветка километража
+
+// --------------------
+// Ветка жрачки
+
 // ====================
 // Подготовка сервера
-const express       = require("express")
+const express    = require("express")
 const bodyParser = require("body-parser")
-const app               = express()
+const app        = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.all('*', (req, res) => {
   res.send(JSON.stringify({"replies": [
-		{"message": main(req.body.query.message)}
+    {"message": main(req.body.query.message)}
   ]}))
 })
 app.listen(process.env.PORT || 3000)
