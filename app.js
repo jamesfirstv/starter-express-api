18a7f945cf3b721c5de8c3858683d07c6eb01984
Correcting <117009969+Correcting@users.noreply.github.com>
2022-10-31 14:36:56 +0100

    update app.js

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function actPath(parse) {
   switch (x) {
     case 2: return '👣Идти дaльше'
     case 22: return '🚷В Темную зону'
-//    case 52: return '🚷В Темную зону'
+//    case 58: return '🚷В Темную зону'
     case 54: return '/voevat_suda'
     case 74: case 75: return '⛺️Вернуться<#>Вернуться в лагерь'
     default: return false
