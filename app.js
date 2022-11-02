9bc10875b64a8f98cf935029e55b2cce71bdf91e
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-11-03 00:33:53 +0100

    update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const mtproto  = new MTProto({
 
 // 2. Print the user country code
 mtproto.call('help.getNearestDc').then(result => {
-  console.log('country:', result.country);
-});
+  console.log('country:', result.country)
+})
 
 
 
