787ae5ce64b4ab512d464b8f3e11fca640eff6b7
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-11-03 03:29:59 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+const CyclicDB = require('cyclic-dynamodb')
+const db = CyclicDB('your-table-name')
+
+const run = async function(){
+    let animals = db.collection('animals')
+
+    // create an item in collection with key "leo"
+    let leo = await animals.set('leo', {
+        type:'cat',
+        color:'orange'
+    })
+
+    // get an item at key "leo" from collection animals
+    let item = await animals.get('leo')
+    console.log(item)
+}
+run()
+
+
+
 const MTProto  = require('@mtproto/core')
 const tempStor = require('@mtproto/core/src/storage/temp')
 const api_id   = YOU_API_ID
