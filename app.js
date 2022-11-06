57f171ec67422c2599a2bfa163af077fc2f35672
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-11-06 14:12:29 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const CyclicDB = require('cyclic-dynamodb')
-//const db = CyclicDB('your-table-name')
+const db = CyclicDB('apricot-calf-garbCyclicDB')
 console.log('ok!')
 
 /*
@@ -57,64 +57,41 @@ function main(msg) {
   ) return '⚔️Атаковать'
 
   if (
-    msg.match('📯🚷 ❤️')
-    || msg.match('📯❤️')
-  ) return 'Двигаться дальше'
-
-  if (
     msg.match('Ты не сможешь увильнуть от противника')
     || msg.match('Тебе не уйти от противника')
     || msg.match('Во время вылазки на тебя напал')
   ) return '⚔️Дать отпор'
 
-  let parse = msg.match(/🚷[\s\S]*👣(\d+)км/)
-  if (parse) parse = actPath(parse, false)
-  if (parse) return parse
-//  parse = msg.match(/🚷.*(\d+)км/)
-//  if (parse) parse = actPath(parse, false)
-//  if (parse) return parse
-//  parse = msg.match(/👣(\d+)км/)
-//  if (parse) parse = actPath(parse)
-//  if (parse) return parse
-  parse = msg.match(/🚷.*(\d+)\s?км/)
-  if (parse) parse = actPath(parse, false)
-  if (parse) return parse
-  parse = msg.match(/👣(\d+)\s?км/)
+
+  let parse = msg.match(/👣(\d+)км/)
   if (parse) parse = actPath(parse)
   if (parse) return parse
 
-
-  if (msg.match('Ты встретил бродячего торговца,'))
-    return '/buy_5i<#>/view'
-
-  if (
-    msg.match('Ты оценил обстановку вокруг.')
-    || msg.match('Ты огляделся вокруг себя.')
-    || msg.match('Рейд в 01:00')
-    || msg.match('Рейд в 9:00')
-    || msg.match('Рейд в 17:00')
-  )
+  if (msg.match('/view'))
     return '👣Идти дaльше'
 
   if (
     msg.match('Ты съел ')
-    || msg.match('/view')
     || msg.match('Ты одержал победу!')
-    || msg.match('Ты готов снова отправиться в Пустошь!')
     || msg.match(' и его')
-    || msg.match('с виду зверька. Это был кот,')
-    || msg.match('— Кис-кис-кис..')
     || msg.match('\n🐐')
     || msg.match('\s🤘')
     || msg.match('(без банды)')
     || msg.match('водохранилище\n 🕳+')
     || msg.match('датацентр\n 🕳+')
-  ) return '/view'
+  ) return '🔎Дeйствие'
 
   if (msg.match('Ты очень голоден.'))
     return '/myfood'
   parse = msg.match(/\/use_1[0-2]\d/g)
   if (parse) return parse[0]
+  
+
+
+  if (msg.match('Ты встретил бродячего торговца,'))
+      return '/buy_5i<#>🔎Дeйствие'
+      
+
 
   
   if (msg.match('/dl_'))
@@ -126,32 +103,17 @@ function main(msg) {
 
 // --------------------
 // Ветка километража
-function actPath(parse, light=true) {
+function actPath(parse) {
   let x = Number(parse[1])
 
-  if (light) {
-    switch (x) {
-      case 2: return '👣Идти дaльше'
-      case 22: return '🚷В Темную зону'
-      case 27: return '👣Идти дaльше'
-      case 11: return 'Старая шахта<#>Двигаться дальше'
-      case 40: return '/eq_54<#>/eq_73'
-      case 45: return '🌁Высокий Хротгар<#>Двигаться дальше'
-      case 50: return '🛑Руины Гексагона<#>Двигаться дальше'
-      case 51: return '🛏Безопасный привал<#>/deeprest'
-      case 52: return '🚷В Темную зону'
-//      case 50: return '/mystuff'
-//      case 54: return '/voevat_suda'
-      case 68: case 69: return '⛺️Вернуться<#>Вернуться в лагерь'
-      default: return false
-    }
-  } else {
-    switch (x) {
-      case 56: return '🔬Научный комплекс<#>Двигаться дальше'
-//      case 39: return '🦇Бэт-пещера<#>Двигаться дальше'
-      case 23: return '🚽Сточная труба<#>Двигаться дальше'
-      default: return false
-    }
+  switch (x) {
+    case 2: return '👣Идти дaльше'
+    case 22: return '🚷В Темную зону'
+    case 52: return '🚷В Темную зону'
+//    case 50: return '/mystuff'
+    case 20: return '/voevat_suda'
+    case 68: case 69: return '⛺️Вернуться<#>Вернуться в лагерь'
+    default: return false
   }
 }
 
@@ -164,10 +126,7 @@ function actClean(msg) {
     'Броня братства',
     'Кинжал',
     'Кожанный нагрудник',
-    'Кожаный жилет',
-    'Титановые щитки',
-    'Мачете',
-    'Шипастая бита',
+    'Мачате',
     'Лазерный тесак',
     'Мото-защита',
     'Плотный капюшон',
@@ -180,7 +139,7 @@ function actClean(msg) {
     'Хлыст',
     'Электромеч'
   ]
-//
+
   let parse
   for (let i=0; i<badGoods.length; i++) {
     parse = msg.match(new RegExp(badGoods[i]+'.*(\\/dl_\\d+)'))
