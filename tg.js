0252b14065042e7f1784a11577d6b8c2211b10b6
jamesfirstv <40821645+jamesfirstv@users.noreply.github.com>
2022-11-08 03:05:01 +0100

    update tg.js

--- tg.js
+++ tg.js
@@ -1,5 +1,4 @@
-const path = require('path')
-const MTProto = require('@mtproto/core')
+const MTProto   = require('@mtproto/core')
 const { sleep } = require('@mtproto/core/src/utils/common')
 
 class API {
